9e02a1af874283e9cfe34a2219afa57f568129d9
Sithu <cithukyaw@gmail.com>
2022-04-06 16:34:23 +0630

    Add domain for Access-Control-Allow-Origin

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
 
 class Cors
 {
@@ -17,7 +18,7 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Origin', 'https://' . Config::get('shopify.shop'))
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     }
 }
