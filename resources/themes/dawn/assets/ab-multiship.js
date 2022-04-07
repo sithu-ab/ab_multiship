08c790a1ef7c80b472861a696862fe7280a30bdf
Sithu <cithukyaw@gmail.com>
2022-04-07 23:40:53 +0630

    Sync cart

--- resources/themes/dawn/assets/ab-multiship.js
+++ resources/themes/dawn/assets/ab-multiship.js
@@ -1,7 +1,15 @@
 (function ($) {
-    const API_BASE = 'https://2f3d-103-203-132-173.ngrok.io/api/storefront/';
+    const API_BASE = 'https://bf20-103-203-132-174.ngrok.io/api/storefront/';
     const $cart = $('#cart');
     const $checkoutBtn = $('#checkout[form="cart"]');
+    const STD_DELIVERY_FEE = 1000;
+    let cartUpdateQueryData = {};
+
+    const address = { // temporary
+        zip: "445-0004",
+        country: "Japan",
+        province: "Kagoshima"
+    };
 
     console.log('cart by ab-multiship');
     console.log($cart);
@@ -35,6 +43,12 @@
 
             if ($.isEmptyObject(AB.cart)) {
                 AB.cart = $.extend(true, {}, AB.shopifyCart);
+                AB.cart.deliveryFee = {};
+                AB.cart.deliveryFeeItem = {
+                    product_title: '通常配送',
+                    price: 0,
+                    quantity: 1
+                };
             }
         } else {
             AB.multishipping = false;
@@ -69,6 +83,7 @@
         let $container = $(this).parent();
         let $div = $container.prev().clone();
         $div.insertBefore($container);
+        $div.find('.abms-qty').val(1);
 
         collectShippingAddress($container.data('index'));
     });
@@ -94,81 +109,158 @@
         });
     }
 
+    function getAddressById(id)
+    {
+        return AB.addresses.find(x => x.id === id);
+    }
+
     function collectShippingAddress(index)
     {
         AB.cart.items[index].shipping = [];
+        const $container    = $('#abms-item-' + index);
+        const variantId     = parseInt($container.find('.abms-variant-id').val());
+        let totalQty        = 0;
 
-        $('#abms-item-' + index + ' .abms-cart-address').each(function (i, elem) {
-            const variantId = parseInt($(elem).find('.abms-variant-id').val());
-            const qty       = parseInt($(elem).find('.abms-qty').val());
-            const addrId    = parseInt($(elem).find('.abms-address').val());
-            const addrLine  = $(elem).find('.abms-address option[value="' + addrId + '"]').text();
+        $container.find('.abms-cart-address').each(function (i, elem) {
+            const qty = parseInt($(elem).find('.abms-qty').val());
+            const addrId = parseInt($(elem).find('.abms-address').val());
 
             if (addrId) {
+                let addr = getAddressById(addrId);
+                if (!addr) {
+                    return;
+                }
+
                 AB.cart.items[index].shipping.push({
                     quantity: qty,
                     variantId: variantId,
                     customAttributes: {
                         id: addrId,
-                        address: addrLine,
+                        address: addr.display,
                     },
-                    shippingAddress: {
-                        address1: addrLine,
-                        address2: '',
-                        city: '',
-                        company: '',
-                        country: '',
-                        firstName: '',
-                        lastName: '',
-                        phone: '',
-                        province: '',
-                        zip: ''
-                    }
+                    shippingAddress: addr
                 })
+
+                // TODO: prepare shipping rates here
+                // prepareShippingRates({
+                //     zip: addr.zip,
+                //     country: addr.country,
+                //     province: addr.province
+                // });
+
+                if (typeof AB.cart.deliveryFee['addr' + addrId] === 'undefined') {
+                    AB.cart.deliveryFee['addr' + addrId] = STD_DELIVERY_FEE;
+                    AB.cart.deliveryFeeItem.price += STD_DELIVERY_FEE;
+                }
             }
-        })
+        });
 
-        getShippingRates();
-        updateShopifyCart();
+        $container.find('.abms-cart-address .abms-qty').each(function (i, elem) {
+            totalQty += parseInt($(elem).val());
+        });
+
+        AB.cart.items[index].quantity = totalQty;
+        cartUpdateQueryData[variantId] = totalQty;
+
+        updateShopifyCart(cartUpdateQueryData);
     }
 
-    // https://shopify.dev/api/ajax/reference/cart#post-locale-cart-update-js
-    function updateShopifyCart()
+    /**
+     * https://shopify.dev/api/ajax/reference/cart#post-locale-cart-update-js
+     * POST /{locale}/cart/update.js
+     * @param data
+     */
+    function updateShopifyCart(data)
     {
-        // POST /{locale}/cart/update.js
-        // $.post(window.Shopify.routes.root + 'cart/update.js', {
-        //     updates: {
-        //         794864053: 2,
-        //         794864233: 3
-        //     }
-        // })
-    }
+        console.log('updateShopifyCart', data);
 
-    // https://shopify.dev/api/ajax/reference/cart#generate-shipping-rates
-    function getShippingRates()
-    {
-        let address = {
-            zip: "445-0004",
-            country: "Japan",
-            province: "Kagoshima"
-        };
-
-        // POST /{locale}/cart/prepare_shipping_rates.json
-        $.post(window.Shopify.routes.root + 'cart/prepare_shipping_rates.json', {
-            shipping_address: address
-        }, function (res) {
-            console.log('prepare_shipping_rates');
-            console.log(res);
-
-            // POST /{locale}/cart/async_shipping_rates.json
-            $.get(window.Shopify.routes.root + 'cart/async_shipping_rates.json', {
-                shipping_address: address
-            }, function (res) {
-                console.log('async_shipping_rates');
-                console.log(res);
-            });
+        $.post(window.Shopify.routes.root + 'cart/update.js', {
+            updates: data
+        }, function () {
+            getShopifyCartToSync();
         });
     }
 
+    /**
+     * https://shopify.dev/api/ajax/reference/cart#get-locale-cart-js
+     * GET /{locale}/cart.js
+     */
+    async function getShopifyCartToSync()
+    {
+        console.log('getShopifyCart');
+        // $.get(window.Shopify.routes.root + 'cart.js', function (data) {
+        //     // Uncaught SyntaxError: Unexpected token ':' at b (jquery.min.js:2:866)
+        //     console.log(data);
+        // });
+
+        let data = await fetch(window.Shopify.routes.root + 'cart.js', {
+            method: 'GET',
+        })
+        .then(response => {
+            return response.json();
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+
+        console.log(data);
+        AB.shopifyCart = data;
+        // Update custom cart price data
+        AB.cart.id = data.token;
+        AB.cart.item_count = data.item_count;
+        AB.cart.item_subtotal_price = data.item_subtotal_price;
+        AB.cart.original_total_price = data.original_total_price;
+        AB.cart.total_price = data.total_price;
+    }
+
+    /**
+     * https://shopify.dev/api/ajax/reference/cart#generate-shipping-rates
+     * POST /{locale}/cart/prepare_shipping_rates.json
+     * @param address
+     */
+    function prepareShippingRates(address)
+    {
+        console.log('prepare_shipping_rates');
+
+        $.post(window.Shopify.routes.root + 'cart/prepare_shipping_rates.json', {
+            shipping_address: address
+        }, function (res) {
+            console.log(res);
+            asyncShippingRates(address);
+        });
+    }
+
+    /**
+     * https://shopify.dev/api/ajax/reference/cart#example-async_shipping_rates-call
+     * POST /{locale}/cart/async_shipping_rates.json
+     * @param address
+     */
+    function asyncShippingRates(address)
+    {
+        console.log('async_shipping_rates');
+
+        $.get(window.Shopify.routes.root + 'cart/async_shipping_rates.json', {
+            shipping_address: address
+        }, function (res) {
+            console.log(res);
+            getShippingRates(address);
+        });
+    }
+
+    /**
+     * https://shopify.dev/api/ajax/reference/cart#get-locale-cart-shipping_rates-json
+     * GET /{locale}/cart/shipping_rates.json
+     * @param address
+     */
+    function getShippingRates(address)
+    {
+        console.log('shipping_rates');
+
+        $.get(window.Shopify.routes.root + 'cart/shipping_rates.json', {
+            shipping_address: address
+        }, function (res) {
+            console.log(res);
+        });
+    }
 
 })(jQuery);
