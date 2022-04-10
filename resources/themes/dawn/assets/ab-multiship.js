ae9ad559895d1b0a4bce7235a9c4b248b872b509
Sithu <sithu@aluto-benli.com>
2022-04-10 10:58:46 +0630

    Create checkout object by admin checkout api on cart submit

--- resources/themes/dawn/assets/ab-multiship.js
+++ resources/themes/dawn/assets/ab-multiship.js
@@ -1,5 +1,5 @@
 (function ($) {
-    const API_BASE = 'https://bf20-103-203-132-174.ngrok.io/api/storefront/';
+    const APP_API_URL = 'https://a2e4-103-203-132-170.ngrok.io/api/app/';
     const $cart = $('#cart');
     const $checkoutBtn = $('#checkout[form="cart"]');
     const STD_DELIVERY_FEE = 1000;
@@ -25,31 +25,23 @@
             AB.multishipping = true;
             $checkoutBtn.attr('type', 'button');
             // when submit button is clicked
-            $checkoutBtn.on('click', function () {
-                alert(AB.cart.items.length);
-            });
+            $checkoutBtn.on('click', checkout);
 
             $('.abms-cart-address').show();
             $('.abms-cart-more-address').show();
             $cart.find('.cart-item__quantity-wrapper').hide();
 
-            $.ajax({
-                url: API_BASE + 'products',
-                type: 'GET',
-                success: function (res) {
-                    console.log(res);
-                }
-            });
-
             if ($.isEmptyObject(AB.cart)) {
                 AB.cart = $.extend(true, {}, AB.shopifyCart);
                 AB.cart.deliveryFee = {};
-                AB.cart.deliveryFeeItem = {
-                    product_title: '通常配送',
-                    price: 0,
-                    quantity: 1
+                AB.cart.deliveryInfo = {
+                    name: '通常配送',
+                    price: 0
                 };
             }
+
+            getShippingZones();
+            getShopifyCartToSync();
         } else {
             AB.multishipping = false;
             $checkoutBtn.attr('type', 'submit');
@@ -134,23 +126,28 @@
                 AB.cart.items[index].shipping.push({
                     quantity: qty,
                     variantId: variantId,
-                    customAttributes: {
+                    attributes: {
                         id: addrId,
                         address: addr.display,
                     },
                     shippingAddress: addr
                 })
 
-                // TODO: prepare shipping rates here
-                // prepareShippingRates({
-                //     zip: addr.zip,
-                //     country: addr.country,
-                //     province: addr.province
-                // });
-
                 if (typeof AB.cart.deliveryFee['addr' + addrId] === 'undefined') {
-                    AB.cart.deliveryFee['addr' + addrId] = STD_DELIVERY_FEE;
-                    AB.cart.deliveryFeeItem.price += STD_DELIVERY_FEE;
+                    // prepareShippingRates({
+                    //     zip: addr.zip,
+                    //     country: addr.country,
+                    //     province: addr.province
+                    // });
+
+                    let fee = getShippingRate(addr.country_code);
+                    console.log(addr.display, fee);
+
+                    AB.cart.deliveryFee['addr' + addrId] = {
+                        title: addr.display,
+                        price: fee
+                    };
+                    AB.cart.deliveryInfo.price += fee;
                 }
             }
         });
@@ -163,6 +160,34 @@
         cartUpdateQueryData[variantId] = totalQty;
 
         updateShopifyCart(cartUpdateQueryData);
+    }
+
+    function getShippingRate(country, weight = 0)
+    {
+        for (let i = 0; i < AB.shippingZones.length; i++) {
+            let zone = AB.shippingZones[i];
+            let ctr = zone.countries.find(x => x.code === country);
+            if (ctr) {
+                if (zone.price_based_shipping_rates.length) {
+                    return zone.price_based_shipping_rates[0].price * 1;
+                } else if (weight_based_shipping_rates.length) {
+                    // TODO: check with weight
+                    return zone.weight_based_shipping_rates[0].price * 1;
+                }
+                break;
+            }
+        }
+
+        return 0;
+    }
+
+    function getShippingZones()
+    {
+        if (AB.shippingZones.length === 0) {
+            $.get(APP_API_URL + 'shipping-zones', function (data) {
+                AB.shippingZones = data;
+            });
+        }
     }
 
     /**
@@ -260,6 +285,65 @@
             shipping_address: address
         }, function (res) {
             console.log(res);
+        });
+    }
+
+    function checkout()
+    {
+        // prepare data json
+        let data = {};
+        data.email = 'dev.alutobenli@gmail.com';
+        data.line_items = [];
+
+        // line items
+        for (let i = 0; i < AB.cart.items.length; i++) {
+            const item = AB.cart.items[i];
+            let row = {
+                variant_id: item.variant_id,
+                quantity: item.quantity,
+                properties: {}
+            };
+
+            if ((typeof item.shipping !== 'undefined' && item.shipping.length === 0) || typeof item.shipping === 'undefined') {
+                alert('One of the shipping addresses is empty!');
+                return;
+            }
+
+            for (let j = 0; j < item.shipping.length; j++) {
+                row.properties['_ab_shipping_address_' + (j + 1)] = item.shipping[j].shippingAddress.display;
+                if (j === 0) {
+                    data.shipping_address = item.shipping[j].shippingAddress;
+                    delete data.shipping_address.id;
+                }
+            }
+
+            row.properties._ab_shipping_details = JSON.stringify(item.shipping);
+            data.line_items.push(row);
+        }
+
+        data.note_attributes = {
+            '_ab_multiship': 1
+        };
+
+        // note
+        let i = 1;
+        let delivery = AB.cart.deliveryFee;
+        for (const key in delivery) {
+            data.note_attributes['_ab_shipping_fee_' + i + ': ' + delivery[key].title] = delivery[key].price;
+            i++;
+        }
+
+        const addrCount = Object.keys(delivery).length;
+        if (addrCount) {
+            data.shipping_address.address2 = ' (+' + (addrCount - 1) + ' more addresses)';
+        }
+
+        console.log(data);
+
+        $.post(APP_API_URL + 'checkout', data, function (res) {
+            if (typeof res.web_url !== 'undefined') {
+                window.location.href = res.web_url;
+            }
         });
     }
 
